6b2a3da44602617fec0f7b5eff62cded7999a1db
Jakob <jakob.beber@gmail.com>
2024-05-13 18:57:08 +0200

    feat: simple section on landing page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import Landing from "@/app/_components/Landing";
 
 export default function Home() {
   return (
     <div>
+      <Landing/>
 
     </div>
   );
