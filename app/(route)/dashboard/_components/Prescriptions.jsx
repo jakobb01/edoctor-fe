45fa636fe86f52b16eff99bc4b6038752d075a5f
Jakob <jakob.beber@gmail.com>
2024-05-25 02:41:24 +0200

    dev: get data and map data on prescriptions page

--- app/(route)/dashboard/_components/Prescriptions.jsx
+++ app/(route)/dashboard/_components/Prescriptions.jsx
@@ -1,19 +1,39 @@
 "use client"
 import React, {useEffect, useState} from "react";
 import {Button} from "@/components/ui/button";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {db_getDoctors} from "@/app/_utils/doctorApi";
 
 export default function Prescriptions() {
 
     const [prescriptionList, setPrescriptionList] = useState([]);
+    const [doctorList, setDoctorList]=useState([]);
+    const [selectedDoctor, setSelectedDoctor]=useState({});
+    const [medicineList, setMedicineList]=useState([]);
+    const [selectedMedicine, setSelectedMedicine]=useState({});
+
 
     useEffect(() => {
-
+        getDoctorList();
+        getMedicineList();
     }, [])
+
+    const getDoctorList=()=>{
+        db_getDoctors().then(resp=>{
+            setDoctorList(resp.data);
+        })
+    }
+
+    const getMedicineList=()=>{
+        db_getDoctors().then(resp=>{
+            setDoctorList(resp.data);
+        })
+    }
 
     return (
         <div>
             <div className="overflow-x-auto m-5">
-                <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
+                <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm text-center">
                     <thead className="ltr:text-left rtl:text-right">
                     <tr>
                         <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Prescription</th>
@@ -30,12 +50,12 @@ export default function Prescriptions() {
                     <tbody className="divide-y divide-gray-200">
                     {!prescriptionList.length > 0 ?
                         <tr>
-                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">John Doe</td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
-                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
+                            <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{'PRS'+1}</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">iBUPROFEN</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">25.5.2024</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">24.6.2024</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">10</td>
+                            <td className="whitespace-nowrap px-4 py-2 text-gray-700">Dr. Gregory House</td>
                             <td className="whitespace-nowrap px-4 py-2">
                                 <Button
                                     className="inline-block px-4 py-2 text-xs font-medium hover:bg-blue-50 hover:text-primary hover:scale-105 transition-all ease-in-out"
@@ -51,7 +71,46 @@ export default function Prescriptions() {
                 </table>
             </div>
             {/*create a new prescription*/}
-            <div>Create new prescription</div>
+            <div className={'m-5 mt-20'}>
+                <h2 className={'font-bold text-lg p-1 bg-blue-50 rounded-lg text-center'}>Request a new prescription</h2>
+                <div className={'grid grid-cols-2'}>
+                    <h2>Medicine:</h2>
+                    <Select
+                        onValueChange={(value) => {
+                            const fullnameDoc = 'Dr. '+value.name+' '+value.surname;
+                            setSelectedMedicine({id: value.id, fullname: fullnameDoc})
+                        }}>
+                        <SelectTrigger className="w-[180px] bg-white">
+                            <SelectValue placeholder="Select medicine" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {doctorList&&doctorList.map((medicine, index)=>(
+                                <SelectItem key={index} value={medicine}>{medicine.name}</SelectItem>
+                            ))}
+
+                        </SelectContent>
+                    </Select>
+                    <h2>Valid from:</h2>
+                    <h2>Quantity:</h2>
+                    <h2>Select your doctor</h2>
+                    {/* get list of doctors and display their names -> on click store id and name of the doctor */}
+                    <Select
+                        onValueChange={(value) => {
+                            const fullnameDoc = 'Dr. '+value.name+' '+value.surname;
+                            setSelectedDoctor({id: value.id, fullname: fullnameDoc})
+                        }}>
+                        <SelectTrigger className="w-[180px] bg-white">
+                            <SelectValue placeholder="Select a doctor" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            {doctorList&&doctorList.map((doctor, index)=>(
+                                <SelectItem key={index} value={doctor}>{'Dr. '+doctor.name+' '+doctor.surname}</SelectItem>
+                            ))}
+
+                        </SelectContent>
+                    </Select>
+                </div>
+            </div>
         </div>
     );
 };
